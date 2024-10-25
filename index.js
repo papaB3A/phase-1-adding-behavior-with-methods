2a72ca0dd2f26e35f7282822c7262ed6b2ff1b7f
papaB3A <papa.akumu@student.moringaschool.com>
2024-10-25 12:21:09 +0300

    Completed phase-1-adding-behavior-with-methods

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Your code here
+class Cat {
+    //...
+    //name..sex properties
+     //constructor
+    constructor(name, sex){
+        this.name= name;
+        this.sex= sex;
+    }
+
+    speak(){
+        return `${this.name} says meow!`;
+    }
+}
+
+class Dog {
+    //...
+    //name..sex instance variable
+    //constructor
+    constructor(name, sex){
+        this.name= name;
+        this.sex= sex;
+    }
+
+    speak(){
+        return `${this.name} says woof!`;
+    }
+}
+
+class Bird {
+    //...
+    //name..sex properties
+    //constructor
+    constructor(name, sex){
+        this.name= name;
+        this.sex= sex;
+    }
+
+    speak(){
+        //return `${name} says meow!`;
+        if(this.sex==="male"){
+            return `It's me! ${this.name}, the parrot!`;
+        }else{
+            return `${this.name} says squawk!`;
+        }
+    }
+}
+
+let myBird= new Bird("Birdie", "female");
+console.log(myBird.speak());
